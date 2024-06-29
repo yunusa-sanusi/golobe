e1650d23968ddc19e4acb0716d332dba437483ca
Yunusa Sanusi <yunusanusi07@gmail.com>
2024-06-29 18:39:31 +0100

    added cors app for frontend requests

--- golobe-backend/core/settings.py
+++ golobe-backend/core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 
     # Internal apps
     'accounts.apps.AccountsConfig',
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
