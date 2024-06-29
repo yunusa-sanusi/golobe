d4c6bf1011202ea576db166edc3c5f72f3d766b8
Yunusa Sanusi <yunusanusi07@gmail.com>
2024-06-29 18:28:13 +0100

    added view permissions to the staff and customer endpoints

--- golobe-backend/accounts/views.py
+++ golobe-backend/accounts/views.py
@@ -1,15 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
-from .models import CustomerProfile, User, Customer, Staff, StaffProfile
+from .models import CustomerProfile, Customer, Staff, StaffProfile
 from .serializers import StaffUserSerializer, CustomerUserSerializer, StaffProfileSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class Staffs(generics.ListCreateAPIView):
     queryset = Staff.objects.all()
     serializer_class = StaffUserSerializer
+    permission_classes = [IsAdminUser]
 
 
-class Customers(generics.ListCreateAPIView):
+class Customers(generics.CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerUserSerializer
 
@@ -17,13 +21,24 @@ class Customers(generics.ListCreateAPIView):
 class Staff(generics.RetrieveUpdateDestroyAPIView):
     queryset = StaffProfile.objects.all()
     serializer_class = StaffProfileSerializer
+    permission_classes = [IsAuthenticated,
+                          DjangoModelPermissions, IsOwnerOrReadOnly]
     lookup_field = 'slug'
 
-    def perform_destroy(self, instance):
-        return self.request.user.delete()
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance.user)
+        return Response({'detail': f'{instance.user.username} has been deleted successfully.'}, status=status.HTTP_200_OK)
 
 
 class Customer(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomerProfile.objects.all()
     serializer_class = StaffUserSerializer
+    permission_classes = [IsAuthenticated,
+                          DjangoModelPermissions, IsOwnerOrReadOnly]
     lookup_field = 'slug'
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance.user)
+        return Response({'detail': f'{instance.user.username} has been deleted successfully.'}, status=status.HTTP_200_OK)
