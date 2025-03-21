1aeac6126b172130ad945aad41026144855a3b4d
Sanusi Yunusa <yunusanusi07@gmail.com>
2025-03-21 12:02:23 +0100

    added the set new password page

--- src/pages/auth/SetPassword.tsx
+++ src/pages/auth/SetPassword.tsx
@@ -1,7 +1,15 @@
+import AuthFormsWrapper from '../../components/auth/AuthFormsWrapper';
+import SetPasswordForm from '../../components/auth/SetPasswordForm';
 import { useMetadata } from '../../hooks/useMetadata';
 
 const SetPassword = () => {
   useMetadata('Set Password');
-  return <div>SetPassword</div>;
+  return (
+    <AuthFormsWrapper
+      component={<SetPasswordForm />}
+      formHeader="Set New Password"
+      formDescription="Your previous password has been reseted. Please set a new password for your account."
+    />
+  );
 };
 export default SetPassword;
